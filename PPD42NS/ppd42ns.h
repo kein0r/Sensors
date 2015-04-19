7f5e070c9cbf3c0833201e866e56c129b9784e4b
kein0r <github@dummgebabbel.de>
2015-04-19 12:48:53 +0800

    Added template files for PPD42NS sensor code

---
+++ PPD42NS/ppd42ns.h
@@ -0,0 +1,40 @@
+/** @ingroup PPD42NS
+ * @{
+ */
+#ifndef PPD42NS_H_
+#define PPD42NS_H_
+/*
+  Copyright (c) 2015 Jan Rüdiger.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Credits:
+*/
+/*******************| Inclusions |*************************************/
+#include <PlatformTypes.h>
+   
+/*******************| Macros |*****************************************/
+
+/*******************| Type definitions |*******************************/
+
+/*******************| Type definitions |*******************************/
+
+/*******************| Global variables |*******************************/
+
+/*******************| Function prototypes |****************************/
+
+
+#endif
+/** @}*/
